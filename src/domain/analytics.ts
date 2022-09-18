8dc35f5792466c53bc44a1e2f533fe07eb7a5b5c
Kieran <>
2022-09-18 14:25:08 +1000

    Don't send stuff to GA

--- src/domain/analytics.ts
+++ src/domain/analytics.ts
@@ -1,6 +1,6 @@
 export function event(event: string, opts: object = {}) {
   // eslint-disable-next-line
-  (window as any).gtag?.("event", event, opts);
+  // (window as any).gtag?.("event", event, opts);
 
-  console.info("Sent event to Google Analytics", { event, ...opts });
+  console.info("event occurred", { event, ...opts });
 }
